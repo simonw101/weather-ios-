9a8e12c9e0e163f1205c0884ba58b56b8ad7293d
simonw101a <simonw101@googlemail.com>
2020-09-02 01:59:20 +0100

    initial commit

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    http://api.openweathermap.org/data/2.5/weather?lat=35.2809&lon=149.1300&appid=c6450fbf674a6265a015d0ca9c1edbc0
 
     override func viewDidLoad() {
         super.viewDidLoad()
